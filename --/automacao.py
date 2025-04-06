bde669ba808cac7b85b9aa8e22d9fb9f1866fbab
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-04-06 19:20:29 -0300

    pyautogui.PAUSE

--- --/automacao.py
+++ --/automacao.py
@@ -3,6 +3,8 @@ import time
 
 # https://pyautogui.readthedocs.io/en/latest/quickstart.html
 
+pyautogui.PAUSE = 0.5 #semrpe dps de cada comando <pyautogui> ele vai fazer uma pausa
+
 # posições da tela
 # print(pyautogui.position())
 # print(pyautogui.size())
@@ -17,4 +19,5 @@ import time
 # # mouse - quantidade cliques
 # pyautogui.click(x=-1390, y=466, clicks=3)
 
-pyautogui.moveTo(x=127, y=519, duration=0.4)
+pyautogui.moveTo(x=-1103, y=395, duration=0.4)
+pyautogui.click()
