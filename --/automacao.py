21315b5f1244c3ec16a158fb30e6694800238a1a
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-04-06 19:15:43 -0300

    move to

--- --/automacao.py
+++ --/automacao.py
@@ -4,18 +4,17 @@ import time
 # https://pyautogui.readthedocs.io/en/latest/quickstart.html
 
 # posições da tela
-print(pyautogui.position())
-print(pyautogui.size())
+# print(pyautogui.position())
+# print(pyautogui.size())
 
-# mouse
-time.sleep(2)
-pyautogui.click(x=-1390, y=466)
+# # mouse
+# time.sleep(2)
+# pyautogui.click(x=-1390, y=466)
+# # mouse - botão direito
+# pyautogui.click(x=-1390, y=466, button="right")
+# # mouse - botão meio
+# pyautogui.click(x=-1390, y=466, button="right")
+# # mouse - quantidade cliques
+# pyautogui.click(x=-1390, y=466, clicks=3)
 
-# mouse - botão direito
-pyautogui.click(x=-1390, y=466, button="right")
-
-# mouse - botão meio
-pyautogui.click(x=-1390, y=466, button="right")
-
-# mouse - quantidade cliques
-pyautogui.click(x=-1390, y=466, clicks=3)
+pyautogui.moveTo(x=127, y=519, duration=0.4)
